c359eb9841d3309ca083e643e3bd8a212f5c80a4
Taipoon <taiki.hirayama.kpu@gmail.com>
2023-02-06 03:11:57 +0900

    プロジェクトディレクトリをきれいにする

--- pyqt6/common/value_error_popup.py
+++
@@ -1,7 +0,0 @@
-from PyQt6.QtWidgets import QMessageBox, QWidget
-
-
-class ValueErrorPopup(QMessageBox):
-    def __init__(self, parent: QWidget, title: str = "値エラー", content: str = "入力値が正しくありません"):
-        super().__init__()
-        super().warning(parent, title, content)
